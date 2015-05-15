b02dae1bb2b51efa90f29b2c9b3a1d1f8217de81
Ubuntu <ubuntu@ip-172-31-17-187.sa-east-1.compute.internal>
2015-05-15 19:44:39 +0000

    Finish example

--- lib/portal.ex
+++ lib/portal.ex
@@ -7,13 +7,62 @@ defmodule Portal do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3])
+      worker(Portal.Door, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defstruct [:left, :right]
+
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+  portal
+  end
+
+  def push_left(portal) do
+    case Portal.Door.pop(portal.right) do
+      :erro -> :ok
+      {:ok, h} -> Portal.Door.push(portal.left, h)
+    end
+  portal  
+  end
+
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
+  end
+
 end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
+  end
+end
+
